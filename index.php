0bb43b6570847434019d190ea79f85744c5f45f3
loriane Koelsch <loriane.ko@gmail.com>
2013-05-31 11:12:25 +0200

    Ajout index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+ <html>
+	<head>
+		<title>Import Base de données</title>
+		<meta charset="utf-8"/>
+	</head>
+	<body>
+		
+	<div>
+	<h1> Esprit -> Innova </h1>
+	<p><a href="esprit_v2/Esprit_innova/export_esprit_v1.php">Export d'esprit </a></p>
+	<p><a href="esprit_v2/Esprit_innova/import_innova_v2.php">Import des xml </a></p>
+	</div>
+	
+	<div>
+	<h1> Esprit -> Claroline </h1>
+	<p><a href="esprit_v2/Esprit_claroline/export_esprit_v1.php">Export d'esprit </a></p>
+	<p><a href="esprit_v2/Esprit_claroline/import_claro_v1.php">Import des xml </a></p>
+	</div>
+	
+	</body>
+</html>
